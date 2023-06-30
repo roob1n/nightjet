237b3c0f567d36697af1611f3614f64a228f73a6
Robin Schmid <roob1n@MacBook-Pro-von-Robin.local>
2023-06-30 08:56:31 +0200

    added buttons

--- nightjet-frontend/src/app/pages/search/components/result-list/result-list.component.ts
+++ nightjet-frontend/src/app/pages/search/components/result-list/result-list.component.ts
@@ -3,6 +3,7 @@ import {MatListModule} from "@angular/material/list";
 import {SearchFilter} from "../search-form/search-form.component";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 export interface Journey {
   title: string;
@@ -21,42 +22,43 @@ export interface Journey {
     MatListModule,
     NgIf,
     NgForOf,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class ResultListComponent {
   isVisible = true;
   journeyList = [
     {
-      title: 'Wien',
-      title2: '',
-      title3: '',
-      link: ''
+      "title": "Wien",
+      "title2": "2023-07-01 10:00 AM", // Arrival date and time
+      "title3": "2023-07-05 03:30 PM", // Departure date and time
+      "link": ""
     },
     {
-      title: 'Zürich',
-      title2: '',
-      title3: '',
-      link: ''
+      "title": "Zürich",
+      "title2": "2023-07-02 09:30 AM",
+      "title3": "2023-07-04 02:45 PM",
+      "link": ""
     },
     {
-      title: 'Hamburg',
-      title2: '',
-      title3: '',
-      link: ''
+      "title": "Hamburg",
+      "title2": "2023-07-03 11:15 AM",
+      "title3": "2023-07-06 04:20 PM",
+      "link": ""
     },
     {
-      title: 'Stockholm',
-      title2: '',
-      title3: '',
-      link: ''
+      "title": "Stockholm",
+      "title2": "2023-07-04 08:45 AM",
+      "title3": "2023-07-07 01:10 PM",
+      "link": ""
     },
     {
-      title: 'Kiruna',
-      title2: '',
-      title3: '',
-      link: ''
-    }];
+      "title": "Kiruna",
+      "title2": "2023-07-05 02:00 PM",
+      "title3": "2023-07-08 10:30 AM",
+      "link": ""
+    }]
 
   showResults(searchFilter: SearchFilter) {
     console.log(searchFilter);
